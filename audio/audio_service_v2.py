ecb4ec97514a52ca3a1c2c7f67443687327c72f0
Martin Oywa <martinoywa2@gmail.com>
2021-08-17 12:54:47 +0300

    Add new audio service utilizing torch

---
+++ audio/audio_service_v2.py
@@ -0,0 +1,109 @@
+import os
+from youtube_search import YoutubeSearch
+import youtube_dl
+import torch
+import torchaudio
+
+import librosa
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+# configuration options
+ydl_opts = {
+    'format': 'worstaudio/worst',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+# parameters
+SAMPLE_RATE = 44100
+NFFT = 1024
+HL = 512
+MELS = 128
+NUMBER_OF_SAMPLES = 200000
+
+# mel spectrogram transformer
+transformer = torchaudio.transforms.MelSpectrogram(
+    sample_rate=SAMPLE_RATE,
+    n_fft=NFFT,
+    hop_length=HL,
+    n_mels=MELS,
+    center=True,
+    pad_mode="reflect",
+    power=2.0,
+    norm='slaney',
+    onesided=True,
+    mel_scale="htk"
+)
+
+# mix down or convert to mono channel
+def mix_down_if_necessary(signal):
+    if signal.shape[0] > 1:
+        signal = torch.mean(signal, dim=0, keepdim=True)
+    return signal
+
+# resample to SAMPLE_RATE
+def resample_if_necessary(signal, sr):
+    if sr != SAMPLE_RATE:
+        resampler = torchaudio.transforms.Resample(
+            orig_freq=sr,
+            new_freq=SAMPLE_RATE
+        )
+        signal = resampler(signal)
+    return signal
+
+# cut signal if longer than NUMBER_OF_SAMPLES
+def cut_if_necessary(signal):
+    if signal.shape[1] > NUMBER_OF_SAMPLES:
+        signal = signal[:, :NUMBER_OF_SAMPLES]
+    return signal
+
+# right pad if necessary if less than NUMBER_OF_SAMPLES
+def right_pad_if_necessary(signal):
+    if signal.shape[1] < NUMBER_OF_SAMPLES:
+        num_to_pad = NUMBER_OF_SAMPLES - signal.shape[1]
+        signal = torch.nn.functional.pad(signal, (0,num_to_pad))
+    return signal
+
+
+def create_mel_spectrogram(id, track, artist, path):
+    """
+    Finds songs on YouTube using track and artist name then
+    saves the song's mel spectrogram.
+    :param id: Deezer Song ID
+    :param track: The song name
+    :param artist: The song artist
+    :param path: path to store downloaded spectrograms
+    :return: song spectrogram
+    """
+
+    search_name = track + " by " + artist
+    res = YoutubeSearch(search_name, max_results=1).to_dict()
+    link = "https://www.youtube.com" + res[0]["url_suffix"]
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+
+    source = os.listdir()
+    for file in source:
+        if file.endswith(".mp3"):
+            print(file)
+            signal, sr = torchaudio.load(file)
+
+            signal = mix_down_if_necessary(signal)
+            signal = resample_if_necessary(signal, sr)
+            signal = cut_if_necessary(signal)
+            signal = right_pad_if_necessary(signal)
+            signal = transformer(signal)
+
+            # save the mel spectrogram
+            plt.imsave(f"{path}/{id}.png", librosa.power_to_db(signal[0].numpy(), ref=np.max), origin="lower", format='png')
+            plt.savefig(f"{path}/{id}", dpi=300)
+            plt.close()
+            os.remove(file)
+            break
